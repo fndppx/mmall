d9a1a14950f6132377c64184fb4915c8c3debb61
魏柯岩 <270022748@qq.com>
2018-08-01 22:54:47 +0800

    add 分类

---
+++ src/main/java/com/mmall/controller/portal/backend/CategoryManageController.java
@@ -0,0 +1,59 @@
+package com.mmall.controller.portal.backend;
+
+
+import com.mmall.common.Const;
+import com.mmall.common.ResponseCode;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+import com.mmall.service.ICategoryService;
+import com.mmall.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/manage/category")
+public class CategoryManageController {
+
+    @Autowired
+    private IUserService iUserService;
+
+    @Autowired
+    private ICategoryService iCategoryService;
+
+    @RequestMapping("add_category.do")
+    @ResponseBody
+    public ServerResponse addCategory(HttpSession session, String categoryName,@RequestParam(value = "parentId",defaultValue = "0") int parentId){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if (user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录");
+        }
+
+        if (iUserService.checkAdminRole(user).isSuccess()) {
+            return iCategoryService.addCategory(categoryName, parentId);
+        }else {
+            return ServerResponse.createByErrorMessage("无权限操作,需要管理员权限");
+        }
+    }
+
+    @RequestMapping("set_category_name.do")
+    @ResponseBody
+    public ServerResponse setCategoryName(HttpSession session,Integer categoryId,String categoryName){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录");
+        }
+        if(iUserService.checkAdminRole(user).isSuccess()){
+            //更新categoryName
+            return iCategoryService.updateCategoryName(categoryId,categoryName);
+        }else{
+            return ServerResponse.createByErrorMessage("无权限操作,需要管理员权限");
+        }
+    }
+
+
+}
